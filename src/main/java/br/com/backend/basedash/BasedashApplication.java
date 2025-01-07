372c04f07d0f896b7c2e2c58038900aeff6b6cee
Andesson Reis <andessonreys@gmail.com>
2025-01-07 02:34:49 -0300

    feat: Initial Commit

---
+++ src/main/java/br/com/backend/basedash/BasedashApplication.java
@@ -0,0 +1,24 @@
+// ===----------------------------------------------------------------------=== //
+// Copyright 2024 Corre Aqui. All rights reserved.
+//
+// This software and associated documentation are proprietary to Corre Aqui. 
+// Unauthorized use, reproduction, modification, or distribution of this code, 
+// in whole or in part, is strictly prohibited without prior written consent from 
+// Corre Aqui.
+//
+// For licensing inquiries or permission requests
+// ===----------------------------------------------------------------------=== //
+
+package br.com.backend.basedash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasedashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasedashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/backend/basedash/BasedashApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.basedash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
